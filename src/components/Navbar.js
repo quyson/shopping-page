4205090d2666bb8f9307c6d6d800667bd04a6887
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-01-23 13:32:35 -0800

    Add navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,11 @@ const Navbar = () => {
         <div>
             <nav>
                 <h1>Title</h1>
-                <h1>Shop</h1>
-                <h1>Search</h1>
-                <h1>Bag</h1>
+                <div className="leftNav">
+                    <h1>Shop</h1>
+                    <h1>Search</h1>
+                    <h1>Bag</h1>
+                </div>
             </nav>
         </div>
     )
